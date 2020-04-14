5aa56f2c14ffaaa07f0526ad7c40334256f33967
M4Marvin <marvinprakash@gmail.com>
2020-04-14 11:17:06 +0530

    Up untill connecting with the database.

---
+++ application/routes.py
@@ -0,0 +1,60 @@
+from application import app,db
+from flask import render_template,request,json,Response
+
+courseData = [{"courseID":"1111","title":"PHP 111","description":"Intro to PHP","credits":"3","term":"Fall,Spring"}, {"courseID":"2222","title":"Java 1","description":"Intro to Java Programming","credits":"4","term":"Spring"}, {"courseID":"3333","title":"Adv PHP 201","description":"Advanced PHP Programming","credits":"3","term":"Fall"}, {"courseID":"4444","title":"Angular 1","description":"Intro to Angular","credits":"3","term":"Fall, Spring"}, {"courseID":"5555","title":"Java 2","description":"Advanced Java Programming","credits":"4","term":"Fall"}]
+
+@app.route("/")
+@app.route("/index")
+@app.route("/home")
+def index():
+    return render_template("index.html", index=True )
+
+@app.route("/login")
+def login():
+    return render_template("login.html", login=True )
+
+@app.route("/courses/")
+@app.route("/courses/<term>")
+def courses(term = "Spring 2020"):
+    return render_template("courses.html", courseData=courseData, courses = True ,term =term)
+
+@app.route("/register")
+def register():
+    return render_template("register.html", register=True)
+
+@app.route("/enrollment", methods = ["GET","POST"])
+def enrollment(): 
+    id = request.form.get("courseID")
+    title = request.form["title"]
+    term = request.form.get("term")
+    
+    print (request.form.get("title"))
+    return render_template("enrollment.html", enrollment=True, data= {"id":id,"title":title,"term":term})    
+
+
+@app.route("/api")
+@app.route("/api/<idx>")
+def api(idx = None):
+    if idx == None:
+        jdata = courseData
+    else:
+        jdata = courseData[int(idx)]
+
+    return Response(json.dumps(jdata), mimetype="application/json")
+
+class User(db.Document):
+    id          =   db.IntField( unique=True )
+    first_name  =   db.StringField( max_length=50 )
+    last_name   =   db.StringField( max_length=50 )
+    email       =   db.StringField( max_length=30 )
+    password    =   db.StringField( max_length=30 )
+
+@app.route("/user")
+def user():
+    #User(id=0, first_name="Marvin", last_name="Prakash", email="marvinprakash@uta.com", password = "123456789").save()
+    #User(user_id=2, first_name="Sneha", last_name="Bansal", email="snehabansal@uta.com", password = "abc123").save()
+    users = User.objects.all()
+    print(users)
+    return render_template("user.html" ,users=users)
+
+    
